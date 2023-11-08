c6d3c30743e25e67bfba068ede4c4534a9a69f79
GauravDhamankar <119730899+GauravDhamankar@users.noreply.github.com>
2023-11-08 08:23:13 +0530

    Add files via upload

---
+++ onetooneuni/src/main/java/com/jspiders/onetooneuni/App.java
@@ -0,0 +1,9 @@
+package com.jspiders.onetooneuni;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ onetooneuni/src/main/java/com/jspiders/onetooneuni/dto/AdharCard.java
@@ -0,0 +1,20 @@
+package com.jspiders.onetooneuni.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class AdharCard {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id ;
+	@Column(unique = true, nullable = false)
+	private long adharNumber ;
+}
